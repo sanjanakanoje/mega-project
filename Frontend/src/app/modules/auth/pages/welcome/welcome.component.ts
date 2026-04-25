423f45e1d8a2541dd61adb33895e0b30f726fe1e
Srushti Kamble <srushtik704@gmail.com>
2026-04-25 12:20:32 +0530

    Welcome Page

---
+++ Frontend/src/app/modules/auth/pages/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-welcome',
+//   imports: [],
+//   templateUrl: './welcome.html',
+//   styleUrl: './welcome.css',
+// })
+// export class WelcomeComponent {}
+
+
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './welcome.html',
+  styleUrls: ['./welcome.css'],   // <-- fix here also
+})
+export class WelcomeComponent {}
